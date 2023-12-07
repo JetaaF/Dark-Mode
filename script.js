337144ffd76d09e4467daa63da106da56b3117be
deondaci <deondaci1@gmail.com>
2023-12-07 07:58:53 -0800

    frist commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let mode = document.getElementById('dark')
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    document.querySelector('h1').toggle.innerHTML= 'bye'
+}
+// toggle,,, clickon edhe rrin, nuk heket po rrin ndryshimi
+// mode.addEventListener('click',terr)
